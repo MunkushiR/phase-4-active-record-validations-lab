714705f03d77564d730483675207937d024d813f
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-06-20 14:12:18 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validate :category, inclusion: {in: ['Fiction','Non-Fiction']}
+private 
+def sufficiently_clickbait_title
+    return if title.blank?
+    clickbait_phrases = ['Won\'t Believe', 'Secret', 'Top', 'Guess']
+    unless clickbait_phrases.any? { |phrase| title.include?(phrase) }
+      errors.add(:title, 'should be sufficiently clickbait-y')
+    end
+  end
+
 end
